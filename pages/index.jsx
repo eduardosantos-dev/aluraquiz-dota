ae5d08a15c7811f7e2e3b9f432dfa26568d0ecfa
Eduardo Santos <diinoprp@gmail.com>
2021-01-27 15:41:21 -0300

    Aula 2

---
+++ pages/index.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { useRouter } from 'next/router';
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizBackground from '../src/components/QuizBackground';
+import QuizLogo from '../src/components/QuizLogo';
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
+export default function Home() {
+  const router = useRouter();
+  const [name, setName] = useState('');
+
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>Dota 2</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <form onSubmit={(e) => {
+              e.preventDefault();
+              router.push(`/quiz?${name}`);
+            }}
+            >
+              <input
+                placeholder="Digite seu nome"
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+              </button>
+            </form>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da galera</h1>
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/eduardosantos-dev" />
+    </QuizBackground>
+  );
+}
