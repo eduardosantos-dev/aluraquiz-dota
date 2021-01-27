ccb2c56beeede67e2a08ba03d59fe1b04ca90a40
Eduardo Santos <diinoprp@gmail.com>
2021-01-27 20:54:27 -0300

    Aula 3

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,23 +1,16 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
 import { useRouter } from 'next/router';
+
 import db from '../db.json';
 import Widget from '../src/components/Widget';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
 import QuizBackground from '../src/components/QuizBackground';
 import QuizLogo from '../src/components/QuizLogo';
-
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
+import QuizContainer from '../src/components/QuizContainer';
+// eslint-disable-next-line import/no-named-as-default
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 
 export default function Home() {
   const router = useRouter();
@@ -37,15 +30,15 @@ export default function Home() {
               router.push(`/quiz?${name}`);
             }}
             >
-              <input
+              <Input
+                name="nomeDoUsuario"
                 placeholder="Digite seu nome"
-                onChange={(e) => {
-                  setName(e.target.value);
-                }}
+                onChange={(e) => setName(e.target.value)}
+                value={name}
               />
-              <button type="submit" disabled={name.length === 0}>
+              <Button type="submit" disabled={name.length === 0}>
                 Jogar
-              </button>
+              </Button>
             </form>
           </Widget.Content>
         </Widget>
---
+++ src/components/Input/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const InputBase = styled.input`
+  width: 100%;
+  padding: 15px;
+  font-size: 14px;
+  border: 1px solid ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.contrastText};
+  background-color: ${({ theme }) => theme.colors.mainBg};
+  border-radius: ${({ theme }) => theme.colors.borderRadius};
+  outline: 0;
+  margin-bottom: 25px;
+`;
+
+// eslint-disable-next-line react/prop-types
+export default function Input({ onChange, placeholder, ...props }) {
+  return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <InputBase onChange={onChange} placeholder={placeholder} {...props} />
+  );
+}
+
+Input.defaultProps = {
+  value: '',
+};
+
+Input.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  placeholder: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string,
+};
